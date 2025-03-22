7a5e77d3f0100cae2d8c83b7d2802a8fd0461b1c
mahaoming2022 <majun13696562158@hotmail.com>
2025-03-22 15:29:40 +0800

    add

--- tmp2.cpp
+++ tmp2.cpp
@@ -1,19 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
 
+#define i64 long long
+#define rep(i,l,r) for(int i=(l);i<=(r);i++)
+#define fdn(i,r,l) for(int i=(r);i>=(l);i--)
+#define pii pair<int,int>
 using namespace std;
 
-typedef long double db;
+typedef long long ll;
+typedef double db;
+typedef __int128 i128;
+
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+std::mt19937_64 rnd64(std::chrono::steady_clock::now().time_since_epoch().count());
+
+int n;
 
 int main()
 {
-    const db e=2.7,pi=2.7;
-    // 知道变量为：
-    // 橙题，黄题，绿题，蓝题，紫题，黑题，ATRT，CFRT，cnt犇犇，cnt回帖，cnt陶片，Uid
-    vector<db> a(12);
-    for(int i=0;i<12;i++) cin>>a[i];
-    db s1=e*e*a[0]+10.0*pi*pi*a[1]+20*e*e*a[2]+200*pi*a[3]+1919*a[4]+114514*a[5];
-    db S=s1*a[6]*a[7]-a[8]*a[9]*pow(e,a[10])/pi+a[11]/e;
-    cout<<S;
+#ifndef ONLINE_JUDGE
+    freopen("in.in","r",stdin);
+    freopen("out.out","w",stdout);
+#endif
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    cin>>n;
+    db ans=0;
+    ll ox=0,oy=0;ll x,y;
+    rep(i,1,n)
+    {
+        
+        cin>>x>>y;
+        ans+=sqrt(1.0*(x-ox)*(x-ox)+1.0*(y-oy)*(y-oy));
+        ox=x,oy=y;
+    }
+    ox=0,oy=0;
+    ans+=sqrt((x-ox)*(x-ox)+(y-oy)*(y-oy));
+    printf("%.10lf",ans);
 }
